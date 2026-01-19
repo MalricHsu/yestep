523bfa12876b5b6c76bcfe6812801ee7cd491c63
DuncanLin <ken21181238@gmail.com>
2026-01-19 22:06:38 +0800

    add btn setting

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router"; // 引入設定檔
-import "./assets/sass/all.scss";
+import "./assets/scss/all.scss";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
