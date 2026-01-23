aa553d929f508e2a756fdbd3864c8f5e22574a91
Malric <qinglun089@google.com>
2026-01-23 12:14:39 +0800

    feat 新增ESLint Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // import { useState } from "react";
 import { Outlet } from "react-router-dom";
-// import Header from "./components/Header";
+// import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
 function App() {
-	return (
-		<>
-			{/* <Header /> */}
-			<Outlet />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      {/* <Nav /> */}
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
