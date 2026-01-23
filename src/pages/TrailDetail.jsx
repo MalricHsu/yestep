825b9d6281214f97e3076938630f71f3ecd83d53
Malric <qinglun089@google.com>
2026-01-23 11:50:25 +0800

    fix 修正src 路徑

--- src/pages/TrailDetail.jsx
+++ src/pages/TrailDetail.jsx
@@ -1,13 +1,5 @@
 const TrailDetail = () => {
-  return (
-    <>
-      <button type="button" className="btn btn-primary-50 rounded-16">
-        按我
-      </button>
-      <h1>我是標題</h1>
-      <p>aaaaaa</p>
-    </>
-  );
+  return <></>;
 };
 
 export default TrailDetail;
