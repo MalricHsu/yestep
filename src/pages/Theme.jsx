38677e84c7cb1291791abfb0f6cce87ca20861c3
mac-sjlu <sjlu78713@gmail.com>
2026-02-02 20:48:49 +0800

    add: 在主題頁加上<Nav />

--- src/pages/Theme.jsx
+++ src/pages/Theme.jsx
@@ -9,6 +9,7 @@ const Theme = () => {
     }, []);
     return (
         <>
+            <Nav />
             <header
                 style={{
                     backgroundImage: `url("${bg02}"), url("https://images.unsplash.com/photo-1533240332313-0db49b459ad6?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
