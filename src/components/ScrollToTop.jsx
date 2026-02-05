d03b57f72d7d1555a2bd23be2f16b8fb13b0197a
mac-sjlu <sjlu78713@gmail.com>
2026-02-05 20:23:14 +0800

    add: ScrollToTop.jsx 的按鈕變色效果

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,20 +1,70 @@
+import { useState, useEffect } from 'react';
+
+import { useLocation } from 'react-router-dom';
+
+// 不同路由對應顏色
+const routeColorMap = {
+    '/search': 'btn-red',
+    '/detail': 'btn-black-600',
+    '/trails': 'btn-black-700',
+    '/theme': 'btn-black-900',
+    '/member': 'btn-black-800',
+};
+
 function ScrollToTop() {
+    // 預設按鈕不顯示
+    const [isVisible, setIsVisible] = useState(false);
+
+    //處理按鈕顏色
+    const location = useLocation();
+
+    const getButtonColor = () => {
+        const { pathname } = location;
+
+        if (pathname.startsWith('/detail')) {
+            return routeColorMap['/detail'];
+        }
+
+        return routeColorMap[pathname] || 'btn-primary-300';
+    };
+
     const handleScrollToTop = () => {
         window.scrollTo({
             top: 0,
-            behavior: 'smooth', // 'smooth' 可以讓畫面平滑地滾動上去
+            behavior: 'smooth',
         });
     };
 
+    useEffect(() => {
+        const toggleVisibility = () => {
+            // 如果滾動超過 300px，就顯示按鈕
+            if (window.scrollY > 300) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener('scroll', toggleVisibility);
+
+        // 清除監聽器
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+
     return (
-        <button
-            type="button"
-            className="to-top-btn p-0 btn btn-primary-300 rounded-100"
-            onClick={handleScrollToTop}
-        >
-            <span className="material-symbols-outlined p-3">keyboard_arrow_up</span>
-        </button>
+        <>
+            {isVisible && (
+                <button
+                    type="button"
+                    className={`to-top-btn p-0 btn ${getButtonColor()} rounded-100`}
+                    onClick={handleScrollToTop}
+                >
+                    <span className="material-symbols-outlined p-3">keyboard_arrow_up</span>
+                </button>
+            )}
+        </>
     );
 }
-
 export default ScrollToTop;
