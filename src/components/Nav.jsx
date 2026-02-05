3b7248547a1d81f5fa3c26c9f5fd648f1b9bea4e
mac-sjlu <sjlu78713@gmail.com>
2026-02-05 22:38:15 +0800

    fix: 修改 Nav.jsx 的padding

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -67,7 +67,7 @@ const Nav = () => {
                         <div className="dropdown header__login d-none d-lg-flex ">
                             <button
                                 type="button"
-                                className="dropdown-btn d-flex align-items-center text-decoration-none border-0 bg-transparent"
+                                className="dropdown-btn d-flex align-items-center text-decoration-none border-0 bg-transparent mb-2"
                                 data-bs-display="static"
                                 data-bs-toggle="dropdown"
                                 aria-expanded="false"
@@ -77,11 +77,11 @@ const Nav = () => {
                                 </span>
                                 <span>登入/註冊</span>
                             </button>
-                            <ul className="dropdown-menu dropdown-menu-end">
+                            <ul className="dropdown-menu dropdown-menu-end px-3 py-2">
                                 <li>
                                     <Link
-                                        className="dropdown-item text-primary-300 px-4 py-2 "
-                                        to="/login"
+                                        className="dropdown-item text-primary-300 px-4 py-2"
+                                        to="/member"
                                     >
                                         會員中心
                                     </Link>
@@ -89,9 +89,9 @@ const Nav = () => {
                                 <li>
                                     <Link
                                         className="dropdown-item text-primary-300 px-4 py-2"
-                                        to="/register"
+                                        to="/login"
                                     >
-                                        註冊
+                                        登入
                                     </Link>
                                 </li>
                             </ul>
