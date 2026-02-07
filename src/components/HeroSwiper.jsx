8ff8dbfcdf65a6a24e4adfc952ef9d814826eced
mac-sjlu <sjlu78713@gmail.com>
2026-02-07 18:15:21 +0800

    add: 新增HeroSwiper.jsx

---
+++ src/components/HeroSwiper.jsx
@@ -0,0 +1,21 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
+function HeroSwiper() {
+    const [carouselData] = useState([
+        {
+            id: 1,
+            image: 'https://images.unsplash.com/photo-1769745241584-be9b8227e126?q=80&w=800&hight=450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+        {
+            id: 2,
+            image: 'https://images.unsplash.com/photo-1769745241584-be9b8227e126?q=80&w=800&hight=450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+        {
+            id: 3,
+            image: 'https://images.unsplash.com/photo-1769745241584-be9b8227e126?q=80&w=800&hight=450&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        },
+    ]);
+
+    return <></>;
+}
