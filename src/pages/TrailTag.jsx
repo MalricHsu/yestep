65fa36d22d2fde6a43974d3b67f58849467ee694
Malric <qinglun089@google.com>
2026-02-07 14:49:06 +0800

    feat 新增toast 區分trailcard

--- src/pages/TrailTag.jsx
+++ src/pages/TrailTag.jsx
@@ -1,22 +1,22 @@
-import axios from 'axios';
+//react套件
 import { useEffect, useState } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
+//第三方套件
+import axios from 'axios';
+//元件
 import Nav from '../components/Nav';
 
 const tagApi = axios.create({ baseURL: 'https://yestep.zeabur.app/' });
-
 const TrailTag = () => {
     const [trails, setTrails] = useState([]);
-
     //抓取那個網頁？後面的字
     const [searchParams] = useSearchParams(); //拿到網址物件
-
-    // console.log(useSearchParams());
     //從物件中取出相對應的key，就會給出值
     const tagName =
         searchParams.get('trail_tags') ||
         searchParams.get('trail_region') ||
         searchParams.get('trail_system') ||
+        searchParams.get('trail_landscape') ||
         '搜尋結果';
 
     useEffect(() => {
